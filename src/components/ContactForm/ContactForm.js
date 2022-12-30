5201d9d373647ed0bba224501c3d9032c5a380f5
Tonya-Shyva <toshash69@gmail.com>
2022-12-30 17:45:12 +0200

    Update ContactForm.js
    1)returned form's controlled elements;
    2)replaced if else =>if...if;
    3)etc

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -9,6 +9,7 @@ import {
   FormLabelStyled,
   BtnStyled,
 } from './ContactForm.styled';
+import { useState } from 'react';
 
 export function ContactForm() {
   const contacts = useSelector(getContacts);
@@ -17,33 +18,55 @@ export function ContactForm() {
   const nameInputId = nanoid();
   const numberInputId = nanoid();
 
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChangeName = e => {
+    const { value } = e.target;
+    setName(value);
+  };
+
+  const handleChangeNumber = e => {
+    const { value } = e.target;
+    setNumber(value);
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
   const handleFormSubmit = e => {
     e.preventDefault();
     // console.log(e);
-    const form = e.target;
-    const contactName = e.target[0].value;
-    const contactNumber = e.target[1].value;
+    const { name, number } = e.target;
+
+    const contactName = name.value;
+    const contactNumber = number.value;
     const contactsLists = [...contacts];
 
-    // console.log(...contacts);
     if (
       contactsLists.find(
         contact => contactName.toLowerCase() === contact.name.toLowerCase()
       )
     ) {
-      toast.warn(`${contactName} is already in contacts. Enter another name`);
-      e.target[0].value = '';
-    } else if (
-      contactsLists.find(contact => contactNumber === contact.number)
-    ) {
       toast.warn(
-        `${contactNumber} is already in contacts. Enter another number`
+        `Name ${contactName} is already in contacts. Enter another name`
       );
-      e.target[1].value = '';
-    } else {
-      dispatch(addNewContact({ id, name: contactName, number: contactNumber }));
-      form.reset();
+      setName('');
+      return;
     }
+    if (contactsLists.find(contact => contactNumber === contact.number)) {
+      toast.warn(
+        `Number ${contactNumber} is already in contacts. Enter another number`
+      );
+      setNumber('');
+      return;
+    }
+
+    dispatch(addNewContact({ id, name: contactName, number: contactNumber }));
+    toast.success(`Contact ${contactName} successfully added`);
+    reset();
   };
 
   return (
@@ -57,6 +80,8 @@ export function ContactForm() {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
+        value={name}
+        onChange={handleChangeName}
       />
 
       <FormLabelStyled htmlFor={numberInputId}>Number</FormLabelStyled>
@@ -68,6 +93,8 @@ export function ContactForm() {
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
+        value={number}
+        onChange={handleChangeNumber}
       />
 
       <BtnStyled type="submit">Add contact</BtnStyled>
